f50b1057dfe5644e33043ef75e16dba996c6baa3
David Hin <davidh@optloggpusrv.cs.adelaide.edu.au>
2019-01-03 16:58:00 +1030

    commit 17

--- ConvAE/cmaesFitnessFunction.m
+++ ConvAE/cmaesFitnessFunction.m
@@ -26,8 +26,8 @@ function [ fitness ] = fitPlusSimABtimesMeanHue(images)
     % Calculate contributions and metric
     [contributions, metricVec] = contribs(images_cell_decoded,metric);
     sorted_contribs = sort(contributions);
-    norm_contribs = norm(contributions, 0.2);
-    norm_fitness = prod(fitnessVec)^(1/2);
+    norm_contribs = norm(contributions, 0.5);
+    norm_fitness = prod(fitnessVec);
 
     % Plotting
     global distInterval;
